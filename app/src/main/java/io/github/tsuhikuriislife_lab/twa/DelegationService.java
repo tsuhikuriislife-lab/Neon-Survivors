2fff524b557268f6d6cd001759547fbec737c25a
--unset <--unset>
2026-09-08 15:06:53 -0500

    Add initial project setup with Gradle configuration and TWA manifest
    - Created gradle.properties for project-wide Gradle settings.
    - Added gradle-wrapper.jar and gradle-wrapper.properties for Gradle wrapper support.
    - Introduced gradlew and gradlew.bat scripts for executing Gradle tasks.
    - Configured local.properties to specify the Android SDK directory.
    - Added manifest-checksum.txt to track the checksum of the manifest.
    - Created settings.gradle to include the app module.
    - Added store_icon.png for application branding.
    - Created twa-manifest.json for TWA configuration with necessary app details.

---
+++ app/src/main/java/io/github/tsuhikuriislife_lab/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.tsuhikuriislife_lab.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
